e956fbd3e42401d7a80285ceaaaee1189fd82be7
Lex von Biedenfeld <avonbied@users.noreply.github.com>
2020-05-03 05:36:51 -0500

    Add test & Arrange src

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,4 +15,20 @@ function* readFile(file) {
     
 }
 
-console.log(...nextNum(12))
+console.log(...nextNum(12));
+
+
+
+
+function name(carname) // carname is stored
+{
+    for (let name of carsParked) // name stores a string from the array
+    {
+        // Test every name in the loop
+        if (carname === name) {
+            return true;
+        }
+    }
+    // 
+    return false;
+}
