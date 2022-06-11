28063606967f60c5517c44248177cbb62e0b2696
Lex <58815784+avonbied@users.noreply.github.com>
2022-06-11 04:49:05 -0500

    Add title to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import Head from 'next/head';
 import Card from '../components/card';
+
+const title = 'Home';
 
 const Page = (): JSX.Element => {
 	return (<>
-		<section style={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+		<Head>
+			<title>{title}</title>
+		</Head>
+		<section style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 			<Card title='Hello &amp; welcome!'>
 				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia modi nemo ab labore, magnam cumque nisi vel minus. Vitae earum doloremque excepturi cum fuga a magni deserunt perferendis quos molestiae.</p>
 				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia modi nemo ab labore, magnam cumque nisi vel minus. Vitae earum doloremque excepturi cum fuga a magni deserunt perferendis quos molestiae.</p>
