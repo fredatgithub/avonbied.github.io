67e7887fda57119a1ec09db6db0523345d4e0974
Lex <58815784+avonbied@users.noreply.github.com>
2022-09-16 10:09:46 -0500

    Update project to use Astro

--- app/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = (): JSX.Element => {
-	return (
-	<footer id="site_foot">
-		<small>&copy; 1999 &ndash; {new Date(Date.now()).getFullYear()} Lex von Biedenfeld</small> | <small>Last updated <time dateTime="2022-06-05">June 5, 2022</time></small>
-	</footer>
-	);
-}
-
-export default Footer;
