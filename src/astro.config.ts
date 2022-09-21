ad485cf7893050180b4a83666310488ff327c471
Lex <58815784+avonbied@users.noreply.github.com>
2022-09-20 20:29:29 -0500

    Update project structure

---
+++ src/astro.config.ts
@@ -0,0 +1,21 @@
+<<<<<<< HEAD:app/src/astro.config.ts
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	server: {
+		host: true
+	},
+	outDir: 'build'
+});
+=======
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	server: {
+		host: true
+	},
+	outDir: 'build'
+});
+>>>>>>> e1043cf (Update project structure):src/astro.config.ts
