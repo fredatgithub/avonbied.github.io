8d52c094604a54d4dd40aee97f0ea3f32c5eabdc
avonbied <58815784+avonbied@users.noreply.github.com>
2022-09-20 21:49:22 -0500

    2 files modified

--- src/astro.config.ts
+++ src/astro.config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	server: {
-		host: true,
-	},
-	outDir: 'build',
-});
+export default defineConfig({});
