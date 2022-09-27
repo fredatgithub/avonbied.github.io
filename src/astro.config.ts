4ac3b82cd899035fc6a7e1448dc50c84a31ed4f9
Lex <58815784+avonbied@users.noreply.github.com>
2022-09-27 17:22:57 -0500

    Revert back to app/src from src/app

--- src/astro.config.ts
+++ src/astro.config.ts
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://avonbied.github.io',
 	integrations: [sitemap()],
-	srcDir: './app',
 	server: {
 		host: true
 	}
